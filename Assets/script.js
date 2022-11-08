8ca301f34bc61aefb4f680dd38d3b8b45ddc6ca1
edyta0106 <edytaradomska@Edytas-MacBook-Pro.local>
2022-11-08 12:59:28 -0700

    added screenshot to Assets folder

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 // Wrap JS code below in the "ready" method to ensure the code is not run until all DOM elements have been created
 $(document).ready(function () {
-  // Created text for current date to be displayed
+  // Create text for current date to be displayed
   $("#currentDay").text(dayjs().format("dddd, MMM D"));
   console.log(dayjs());
 
@@ -8,14 +8,19 @@ $(document).ready(function () {
   $("#hour").text(dayjs().hour());
   console.log(dayjs().hour()); //lists the current hour in 24-hr format in dev tools
 
-  //
+  // Create button "on" method to save user's text input
   var saveBtn = $(".saveBtn");
   saveBtn.on("click", function (event) {
     event.preventDefault();
+    // timeBlock is the key
     var timeBlock = parseInt($(this).parent().attr("id").split("-")[1]);
+    // textInput is the value
     var textInput = $(this).siblings("textarea").val();
+    // Set key and value in local storage
     localStorage.setItem(timeBlock, textInput);
   });
+
+  // Return the current value of text area from each id nested in html
   $("#hour-9 textarea").val(localStorage.getItem("9"));
   $("#hour-10 textarea").val(localStorage.getItem("10"));
   $("#hour-11 textarea").val(localStorage.getItem("11"));
@@ -28,10 +33,12 @@ $(document).ready(function () {
 
   // Create a function to apply color to past, present, or future class on each timeblock
   function timeBlockColor() {
+    // Create a function for each time-block class so that each hour block changes colors depending on what time it is
     $(".time-block").each(function () {
       var currentHourBlock = parseInt($(this).attr("id").split("-")[1]);
       var currentTime = dayjs().hour();
 
+      // Use an if statement to determine what color each hour block changes to by using the past present or future classes and the current time
       if (currentHourBlock < currentTime) {
         $(this).children("textarea").addClass("past");
       } else if (currentHourBlock == currentTime) {
