8959de78f083e5b95c50d38e845df4b8ac235ae0
edyta0106 <edytaradomska@Edytas-MacBook-Pro.local>
2022-11-08 12:55:03 -0700

    added comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,42 +1,45 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
-// $(function () {
+// Wrap JS code below in the "ready" method to ensure the code is not run until all DOM elements have been created
+$(document).ready(function () {
+  // Created text for current date to be displayed
+  $("#currentDay").text(dayjs().format("dddd, MMM D"));
+  console.log(dayjs());
 
-// TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
+  // Lists current hour
+  $("#hour").text(dayjs().hour());
+  console.log(dayjs().hour()); //lists the current hour in 24-hr format in dev tools
 
-// Created text for current date to be displayed
-$("#currentDay").text(dayjs().format("dddd, MMM D"));
-console.log(dayjs());
-
-// Lists current hour
-$("#hour").text(dayjs().hour());
-console.log(dayjs().hour()); //lists the current hour in 24-hr format in dev tools
-
-// ??? Save text to local storage ???
-var textInput = $("<textarea>");
-function renderText(event) {
-  event.preventDefault();
-  var text = localStorage.getItem("textInput");
-  textInput.text = text;
-}
-
-// ??? Need to add 'this' selector ???
-var saveBtn = $(".saveBtn");
-saveBtn.on("click", function () {
-  localStorage.setItem("text", textInput);
-  renderText();
-});
-
-// Create a function to apply color to past, present, or future class on each timeblock
-function timeBlockColor() {
-  $(".time-block").each(function () {
-    var pastBlock = $(".past")
-    var currentBlock = $(".present");
-    var futureBlock = $(".future")
-    var currentHour = $(this).attrr(“id”).split(“-“)[1]
+  //
+  var saveBtn = $(".saveBtn");
+  saveBtn.on("click", function (event) {
+    event.preventDefault();
+    var timeBlock = parseInt($(this).parent().attr("id").split("-")[1]);
+    var textInput = $(this).siblings("textarea").val();
+    localStorage.setItem(timeBlock, textInput);
   });
-}
+  $("#hour-9 textarea").val(localStorage.getItem("9"));
+  $("#hour-10 textarea").val(localStorage.getItem("10"));
+  $("#hour-11 textarea").val(localStorage.getItem("11"));
+  $("#hour-12 textarea").val(localStorage.getItem("12"));
+  $("#hour-13 textarea").val(localStorage.getItem("13"));
+  $("#hour-14 textarea").val(localStorage.getItem("14"));
+  $("#hour-15 textarea").val(localStorage.getItem("15"));
+  $("#hour-16 textarea").val(localStorage.getItem("16"));
+  $("#hour-17 textarea").val(localStorage.getItem("17"));
 
-// TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
-// TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
+  // Create a function to apply color to past, present, or future class on each timeblock
+  function timeBlockColor() {
+    $(".time-block").each(function () {
+      var currentHourBlock = parseInt($(this).attr("id").split("-")[1]);
+      var currentTime = dayjs().hour();
 
-// });
+      if (currentHourBlock < currentTime) {
+        $(this).children("textarea").addClass("past");
+      } else if (currentHourBlock == currentTime) {
+        $(this).children("textarea").addClass("present");
+      } else {
+        $(this).children("textarea").addClass("future");
+      }
+    });
+  }
+  timeBlockColor();
+});
